f515f6f4c1466730316f09a22f8fc2765f8e8ea7
RubiusCaesar <86538960+RubiusCaesar@users.noreply.github.com>
2021-08-13 20:29:42 -0500

    commit

--- practice/files/filesproject.py
+++ practice/files/filesproject.py
@@ -13,18 +13,18 @@ with open('pythondev/practice/files/passwords.csv') as password_file:
     for password_row in parsed_csv:
         compromised_users.append((password_row['Username']))
 
-with open('compromised_users.txt', 'w') as compromised_user_file:
+with open('pythondev/practice/files/compromised_users.txt', 'w') as compromised_user_file:
     for compromised_user in compromised_users:
         compromised_user_file.write(compromised_user)
 
-with open('boss_message.json', 'w') as boss_message:
+with open('pythondev/practice/files/boss_message.json', 'w') as boss_message:
     boss_message_dict = {
         'recipient': 'The Boss',
         'message': 'Mission Success'
     }
     json.dump(boss_message_dict, boss_message)
 
-with open('new_passwords.csv', 'w') as new_passwords_obj:
+with open('pythondev/practice/files/new_passwords.csv', 'w') as new_passwords_obj:
     slash_null_sig = """
   _  _     ___   __  ____             
 / )( \   / __) /  \(_  _)            
